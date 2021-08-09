15765450ad7830d3a4c404ec942f8825266f5721
HagiAyato <h8a9g11i.@gmail.com>
2021-08-09 20:19:25 +0900

    VScodeでの実装環境構築

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OfficeImg
+{
+    class Program
+    {
+        /// <summary>
+        /// メイン関数
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World{0}!", args[0]);
+        }
+    }
+}
